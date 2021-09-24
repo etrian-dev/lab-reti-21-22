b9489d1fdc38dcd8becaeeb9a6642dd5c3d386de
etrian-dev <nicola.vetrini@gmail.com>
2021-09-24 22:50:56 +0200

    Updated assignment 2: better client handling

--- assignments/assignment_postoffice/Person.java
+++ assignments/assignment_postoffice/Person.java
@@ -11,12 +11,13 @@ public class Person implements Runnable {
 
     public void run() {
         System.out.printf("Cliente %d: entrato nello sportello\n", this.queueNumber);
-        // aspetta da 0 a 1000 ms
+        // aspetta da 0 a 1000 ms (esegue l'operazione allo sportello)
         try {
             Thread.sleep(Math.round(Math.random() * 1000));
         } catch (InterruptedException ex) {
-            ;
+            System.out.printf("Cliente %d: sono stato interrotto\n", this.queueNumber);
         }
-        System.out.printf("Cliente %d: operazione conclusa: esco dallo sportello\n", this.queueNumber);
+        System.out.printf("Cliente %d: operazione conclusa: esco dallo sportello\n",
+                this.queueNumber);
     }
 }
--- assignments/assignment_postoffice/PostOffice.java
+++ assignments/assignment_postoffice/PostOffice.java
@@ -1,33 +1,53 @@
 import java.util.concurrent.*;
-import java.util.Vector;
 
 public class PostOffice extends Thread {
     private ThreadPoolExecutor sportelli;
-    private Vector<Person> clientQueue;
+    private BlockingQueue<Person> clientQueue;
 
-    public PostOffice(long roomCap, Vector<Person> clients) {
+    public PostOffice(long roomCap, BlockingQueue<Person> clients) {
         // 4 sportelli => 4 thread
-        this.sportelli = new ThreadPoolExecutor(4, 4, 0, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>((int)roomCap));
+        this.sportelli = new ThreadPoolExecutor(4, 4, 0, TimeUnit.SECONDS,
+                new ArrayBlockingQueue<Runnable>((int) roomCap));
         // la coda di clienti in attesa di accedere alla seconda sala (unbounded)
         this.clientQueue = clients;
     }
 
     public void run() {
-        while(this.clientQueue.size() > 0) {
-            Person p = this.clientQueue.remove(0);
+        while (this.clientQueue.size() > 0) {
+            // esamino la testa della coda della prima sala
+            Person p = this.clientQueue.peek();
             try {
-                this.sportelli.execute(p);
+                if (p != null) {
+                    // cerco di far passare il cliente nella seconda sala, ma se è piena
+                    // viene lanciata l'eccezione RejectedExecutionException. Altrimenti
+                    // devo rimuovere la task dalla coda della prima sala, perché è stata accettata
+                    this.sportelli.execute(p);
+                    this.clientQueue.take();
+                    System.out.printf("Cliente %d: entrato nella sala interna\n", p.myNumber());
+                }
             } catch (RejectedExecutionException noex) {
-                System.out.printf("Cliente %d: Sala interna piena\n", p.myNumber());
+                System.out.printf("Cliente %d: Sala interna piena: aspetto 50ms\n", p.myNumber());
+                try {
+                    Thread.sleep(50);
+                } catch (Exception e) {
+                    System.out.printf("Cliente %d: Risvegliato\n", p.myNumber());
+                }
+            } catch (InterruptedException ex) {
+                // potrei ricevere un interrupt anche mentre sto rimuovendo il cliente dalla coda della prima sala
+                System.out.println("Cliente %d: Interrotto durante la rimozione dalla coda");
             }
         }
+
+        // la coda della prima sala è vuota, per cui lancio lo shutdown e faccio in modo da far terminare tutti i thread presenti
         this.sportelli.shutdown();
-        while(!this.sportelli.isShutdown()) {
+        if (!this.sportelli.isShutdown()) {
             try {
-                Thread.sleep(10);
-            } catch (Exception e) {
-                //TODO: handle exception
-                ;
+                // potrebbe essere interrotto durante l'attesa di terminazione dei thread del pool, ma è improbabile
+                this.sportelli.awaitTermination(1, TimeUnit.HOURS);
+            } catch (InterruptedException e) {
+                System.out.println(
+                        "Thread interrotto durante attesa shutdown threadpool. Chiamo shutdownNow()");
+                this.sportelli.shutdownNow();
             }
         }
     }
